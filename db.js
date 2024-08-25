8167f5b6de39fb8b52bd2abfe101f2c39ab69b34
Shahin Bano <banoshahin89@gmail.com>
2024-08-25 16:54:12 +0530

    mongodb url change

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://127.0.0.1:27017/post-app")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ Connected to mongoDB");
   })
